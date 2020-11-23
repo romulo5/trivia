68c4402e115b8721ebc8e8fc105c546ede1c8738
Romulo Melo <romulofloresta@gmail.com>
2020-11-23 20:38:57 -0300

    Add style to Home Screen
    Just some  basic css over the home screen html

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import '../styles/tailwind.output.css';
 
 const App = () => (
-  <div className="App ">
-    <header className="App-header">
-      <h1 className="text-red-400">Welcome to the Trivia Challenge</h1>
-    </header>
+  <div className="flex flex-col justify-center items-center bg-gray-700 h-screen">
+    <h1 className="text-white text-xl">Welcome to the Trivia Challenge</h1>
+    <p className="text-gray-300 p-6">
+      You be presented with 10 True or False questions
+    </p>
+    <p className="text-gray-300">Can You score 100% ?</p>
+    <button
+      type="submit"
+      className="p-6 bg-green-400 px-6 py-2 mt-4 shadow-xl rounded-mdv "
+    >
+      Begin
+    </button>
   </div>
 );
 
